a8db52bef373f5943189686e216fc5cc7e37a777
Jewel-Anne <reyesjewelanne232@gmail.com>
2024-03-02 20:34:58 +0800

    add coding exercise 2.11

---
+++ Programming with Python/1 Programming for Beginners Using Python/Module 2 Coding 2.11.py
@@ -0,0 +1,24 @@
+# Instructions:
+# Read each number carefully, then perform the task required.
+# Make sure to use the print() function
+# 1.) Create a boolean statement to determine whether ten (10) is greater than seven (7).
+# 2.) Convert the numerical value 73911 into a string.
+# 3.) Convert the string "Thank God it's Friday!" into a tuple.
+# 4.) Convert 4302 into a floating number.
+# 5.) Convert 3299.35640 into an integer.
+
+
+# 1.) Create a boolean statement to determine whether ten (10) is greater than seven (7).
+print(10>7)
+
+# 2.) Convert the numerical value 73911 into a string.
+print(str(73911))
+
+# 3.) Convert the string "Thank God it's Friday!" into a tuple.
+print(tuple("Thank God it's Friday!"))
+
+# 4.) Convert 4302 into a floating number.
+print(float(4302))
+
+# 5.) Convert 3299.35640 into an integer.
+print(int(3299.35640))
