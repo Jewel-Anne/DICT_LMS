a0f58531566041c94a2d92d0d6a081c03dec8fba
Jewel-Anne <reyesjewelanne232@gmail.com>
2024-03-02 17:02:19 +0800

    add Module 1 & 2 Py

---
+++ DICT Learning Management System/Programming with Python/1 Programming for Beginners Using Python/Module 1 Code Exer 1.14.py
@@ -0,0 +1,37 @@
+# Instructions
+# Check each line of code and try to figure out what the errors are.
+# Modify the code to let it run successfully.
+# 1. printRoses are red, violets are blue
+# 2. if 10>7:
+#    print("Ten is greater than seven!")
+#    if 16 < 42:
+#    print("Sixteen is less than forty-two!")
+# 3. print("A long time ago in a galaxy far, far away...");
+
+###############################################
+# 1. printRoses are red, violets are blue
+# the syntax is wrong. there is no parenthesis and quotation marks
+# to indicate to print the texts.
+# Instead, it should be like this:
+
+print("Roses are red, violets are blue.")
+
+###############################################
+# 2. if 10>7:
+#    print("Ten is greater than seven!")
+#    if 16 < 42:
+#    print("Sixteen is less than forty-two!")
+# there should be indentation for blocks of code.
+# Instead, it should look like this:
+
+if 10 > 7:
+    print("Ten is greater than seven!")
+if 16 < 42:
+    print("Sixteen is less than forty-two!")
+
+###############################################
+# 3. print("A long time ago in a galaxy far, far away...");
+# there is no need for a semicolon for python syntax.
+# Instead, it should be like this:
+
+print("A long time ago in a galaxy far, far away...")
